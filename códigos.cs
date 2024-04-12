7bf3503b0970740f84db10910deca73c4f3a5bff
Ruan Pablo Silva <161895419+ruanpablosilva@users.noreply.github.com>
2024-04-12 03:57:41 -0300

    Add files via upload

---
+++ códigos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAperte_Click(object sender, EventArgs e)
+        {
+            if( cboCidades.SelectedItem.ToString() == "Sydney" )
+            {
+                lblResultado.Text = "Você ama a cidade das praias!";
+            }
+            else if ( cboCidades.SelectedItem.ToString() == "Paris" )
+            {
+                lblResultado.Text = "Você ama a cidade da luz!";
+            } 
+            else if ( cboCidades.SelectedItem.ToString() == "Roma")
+            {
+                lblResultado.Text = "Você ama a cidade eterna!";
+            }
+            else if ( cboCidades.SelectedItem.ToString() == "São Paulo" )
+            {
+                lblResultado.Text = "Você tem um bom gosto para viagens!";
+            }
+        }
+    }
+}
+//1 ComboBox, 1 label(para o resultado) e 1 button. adicionei as cidades na combobox pelo "items" na caixa de propriedades mas o comando manual é esse:
+  //{
+//comboBox1.Items.Add("Sydney");
+//comboBox1.Items.Add("Paris");
+//comboBox1.Items.Add("Roma");
+        //}
+
+//private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+
+        //{
+   // string selectedOption = comboBox1.SelectedItem.ToString();
+   // MessageBox.Show("Opção selecionada: " + Sydney); 
+ //}
